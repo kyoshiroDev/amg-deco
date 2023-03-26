39e8bc1db14ff6acd4eb07ca4ca15021a825cfde
Kyoshiro laptotdev <tahir.gregory@gmail.com>
2023-03-26 18:43:46 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+
+// import Home from "./components/Home/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const routes = [
+		{ titre: "ACCUEIL" },
+		{ titre: "RÉALISATION" },
+		{ titre: "PRESTATIONS ET TARIFS" },
+		{ titre: "À PROPOS" },
+		{ titre: "CONTACT" },
+	];
+
+	return (
+		<div className="App">
+			<Header routes={routes} />
+			<main className="main-contenaire">
+				<h1>Mon Site</h1>
+			</main>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+	return (
+		<div className="container-home">
+			<h1>Bienvenue sur notre agence web</h1>
+		</div>
+	);
+}
+
+export default Home;
